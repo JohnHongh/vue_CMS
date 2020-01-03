c7d6d47272f050cf0d68afba1f848e61670cc8f5
陈剑宇 <102323845@qq.com>
2020-01-03 22:58:38 +0800

    feat:add axios cancel

--- vue/src/api/api.js
+++ vue/src/api/api.js
@@ -1,30 +1,45 @@
 import axios from 'axios';
 import config from './api.config';
+
+let CancelToken = axios.CancelToken;
+let cancel;
+let urlCancel = {};
+
 const request = axios.create({
   baseURL: process.env.NODE_ENV === 'development' ? config.BASE_URL_DEV : config.BASE_URL_PROD,
   timeout: config.TIME_OUT,
   'Content-Type': 'application/json;charset=UTF-8',
+  cancelToken: new CancelToken(function executor(c) {
+    cancel = c;
+  })
 });
 
 request.interceptors.request.use(function (config) {
-  // 在发送请求之前做些什么
+  if(urlCancel[config.url]){
+    urlCancel[config.url]('取消请求');
+    urlCancel[config.url] = cancel;
+  }else{
+    urlCancel[req.url] = cancel;
+  }
   console.log(config)
   config.headers.token = 'token';
   return config;
 }, function (error) {
-  // 对请求错误做些什么
   return Promise.reject(error);
 });
 
 request.interceptors.response.use(function (response) {
-  // 对响应数据做点什么
-  console.log(response)
+  if(urlCancel[res.url]){
+    delete urlCancel[res.url];
+  }
   if (response.data.code === -1) {
     return Promise.reject(response.data.msg);
   }
   return response;
 }, function (error) {
-  // 对响应错误做点什么
+  if(urlCancel[res.url]){
+    delete urlCancel[res.url];
+  }
   return Promise.reject(error);
 });
 
